8014b06ae7f531f198d22f4c255337780f7e0aff
IvanTZ <zaychev010@gmail.com>
2022-12-10 02:48:06 +0100

    commit entrega final

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,31 +1,60 @@
 import 'package:flutter/cupertino.dart';
-import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
+import 'package:movies_app/models/models.dart';
+import 'package:movies_app/models/popular_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
-  String _baseUrl = "api.themoviedb.org";
-  String _apiKey = "db6e8e03a0180d0a9a316ed631bbb75f";
-  String _language = "es-Es";
-  String _page = "1";
+  final String _baseUrl = 'api.themoviedb.org';
+  final  String _apiKey = 'db6e8e03a0180d0a9a316ed631bbb75f';
+  final String _language = 'es-Es';
+  final String _page = '1';
+
+  List<Movie> onDisplayMovies = [];
+  List<Movie> popularMovies = [];
+
+  Map<int, List<Cast>> casting = {};
+
   MoviesProvider() {
     print('Movies Provider inicializat');
-    this.getOnDisplayMovies();
+    getOnDisplayMovies();
+    getPopularMovies();
   }
 
   getOnDisplayMovies() async {
     print('getOnDisplayMovies');
-    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing',
         {'api_key': _apiKey, 'language': _language, 'page': _page});
+    var response = await http.get(url);
+
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    onDisplayMovies = nowPlayingResponse.movies;
+
+    notifyListeners();
+  }
+
+  getPopularMovies() async {
+    print('getPopulars');
+    var url = Uri.https(_baseUrl, '3/movie/popular',
+        {'api_key': _apiKey, 'language': _language, 'page': _page});
+    var response = await http.get(url);
+
+    final popularResponse = PopularResponse.fromJson(response.body);
+
+    popularMovies = popularResponse.moviePops;
+
+    notifyListeners();
+  }
+
+  Future<List<Cast>> getMovieCast(int idMovie) async {
+    print('getCast');
+    var url = Uri.https(_baseUrl, '3/movie/$idMovie/credits',
+        {'api_key': _apiKey, 'language': _language});
 
     // Await the http get response, then decode the json-formatted response.
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      var jsonResponse =
-          convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['totalItems'];
-      print('Number of books about http: $itemCount.');
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
+    final result = await http.get(url);
+    final creditsResponse = CreditResponse.fromJson(result.body);
+    casting[idMovie] = creditsResponse.cast;
+
+    return creditsResponse.cast;
   }
 }
